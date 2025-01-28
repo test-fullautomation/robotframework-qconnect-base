5939c4c6440ac2ce66655aefb9bff6d6456ae3ba
qth2hi <holger.queckenstedt@de.bosch.com>
2025-01-28 12:06:49 +0100

    Selftests added, debug info in connection_manager.py added

--- test/tcp_ip/imports/tcp_ip_selftest_lib.py
+++ test/tcp_ip/imports/tcp_ip_selftest_lib.py
@@ -43,8 +43,8 @@ from PythonExtensionsCollection.String.CString import CString
 # --------------------------------------------------------------------------------------------------------------
 
 THISMODULENAME    = "tcp_ip_selftest_lib.py"
-THISMODULEVERSION = "0.5.0"
-THISMODULEDATE    = "27.01.2025"
+THISMODULEVERSION = "0.5.1"
+THISMODULEDATE    = "28.01.2025"
 THISMODULE        = f"{THISMODULENAME} v. {THISMODULEVERSION} / {THISMODULEDATE}"
 
 TESTSERVER_TIME_TO_QUIT = 3
@@ -189,8 +189,8 @@ class tcp_ip_selftest_lib():
             finished_pid = None
             if hasattr(os, 'WNOHANG'): # not available in all os, but on Linux this avoids zombie processes, because it forces to catch the status
                 try:
-                    finished_pid, status = os.waitpid(server_pid, os.WNOHANG)
-                    msg = f"process {finished_pid} finished with status {status}"
+                    finished_pid, status = os.waitpid(server_pid, os.WNOHANG) # (why is finished_pid = 0?)
+                    msg = f"process {server_pid} finished with status {status}"
                     BuiltIn().log(msg, level="INFO", console=True)
                 except ChildProcessError as ex:
                     BuiltIn().log(f"{ex}", level="INFO", console=True)
