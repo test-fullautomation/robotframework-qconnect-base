fdf2699033f85387ecaf0af0dde7a59b5862f537
ngoan1608 <ngoan1608@gmail.com>
2025-06-16 16:39:46 +0700

    reverted to source code that works with GM project

--- QConnectBase/version.py
+++ QConnectBase/version.py
@@ -12,5 +12,5 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-VERSION      = '1.1.5'
-VERSION_DATE = '02.06.2025'
+VERSION      = '1.1.6'
+VERSION_DATE = '16.06.2025'
